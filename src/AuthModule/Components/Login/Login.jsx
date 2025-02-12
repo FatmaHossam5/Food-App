5b4eeb42fcbe2a6997862343e80b85a969b67391
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2025-02-12 15:11:20 +0200

    starting enhance Code

--- src/AuthModule/Components/Login/Login.jsx
+++ src/AuthModule/Components/Login/Login.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import { Link, useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AuthForm from '../../../Components/SharedUI/AuthForm'
 
 
 
@@ -14,7 +15,7 @@ export default function Login({saveAdminData}) {
     const navigate=useNavigate()
     const[isLoading,setIsLoading]=useState(false)
 const Login =(data)=>{
-axios.post('https://upskilling-egypt.com:443/api/v1/Users/Login',data)
+axios.post('https://upskilling-egypt.com:3006/api/v1/Users/Login',data)
 
 .then((response)=>{
   
@@ -35,43 +36,44 @@ axios.post('https://upskilling-egypt.com:443/api/v1/Users/Login',data)
 
 
   return (
-    <div className="Auth-container">
+    // <div className="Auth-container">
       
             
-        <div className="row bg-overlay  vh-100">
-            <div className="col-md-6 m-auto">
-                <div className="bg-white p-2" >
-                    <div className="img text-center ">
-                        <img src={logo} className='w-50' alt="logo" />
-                    </div>
+    //     <div className="row bg-overlay  vh-100">
+    //         <div className="col-md-6 m-auto">
+    //             <div className="bg-white p-2" >
+    //                 <div className="img text-center ">
+    //                     <img src={logo} className='w-50' alt="logo" />
+    //                 </div>
                   
-                    <form className='w-75 m-auto' onSubmit={handleSubmit(Login)} >
+    //                 <form className='w-75 m-auto' onSubmit={handleSubmit(Login)} >
               
-                    <h3>Log In</h3>
-                    <p className='text-color'>welcome Back!Please enter your details</p>
-                        <div className="form-group my-3">
-                        <input className='form-control email  px-4 ' type="email" placeholder='Enter your E-mail'
-                        {...register("email",{required:true,pattern:/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/})}
-                         />
-                         {errors.email&&errors.email.type==="required"&&(<span className='text-danger'>email is required</span>)}
-                         <i className="fa-solid fa-mobile left-icon"></i>
-                        </div>
-                        <div className="form-group my-3">
-                       <input className='form-control px-4' type="password" placeholder='Password' 
-                       {...register("password",{required:true,pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d!@#$%^&*()_+]{6,}$/})}/>
-                           {errors.password&&errors.password.type==="required"&&(<span className='text-danger'>Password Is required</span>)}
-                           <i className="fa-solid fa-lock left-icon"></i>
-                        </div>
-                        <div className="form-group  my-3 position-relative d-flex justify-content-end ">
-                            <Link to ="/RequestResetPassword" className='text-success text-decoration-none '> Forget Password? </Link>
+    //                 <h3>Log In</h3>
+    //                 <p className='text-color'>welcome Back!Please enter your details</p>
+    //                     <div className="form-group my-3">
+    //                     <input className='form-control email  px-4 ' type="email" placeholder='Enter your E-mail'
+    //                     {...register("email",{required:true,pattern:/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/})}
+    //                      />
+    //                      {errors.email&&errors.email.type==="required"&&(<span className='text-danger'>email is required</span>)}
+    //                      <i className="fa-solid fa-mobile left-icon"></i>
+    //                     </div>
+    //                     <div className="form-group my-3">
+    //                    <input className='form-control px-4' type="password" placeholder='Password' 
+    //                    {...register("password",{required:true,pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d!@#$%^&*()_+]{6,}$/})}/>
+    //                        {errors.password&&errors.password.type==="required"&&(<span className='text-danger'>Password Is required</span>)}
+    //                        <i className="fa-solid fa-lock left-icon"></i>
+    //                     </div>
+    //                     <div className="form-group  my-3 position-relative d-flex justify-content-end ">
+    //                         <Link to ="/RequestResetPassword" className='text-success text-decoration-none '> Forget Password? </Link>
                             
-                        </div>
-                      <button className='bg-success form-control text-white logBtn' disabled={isLoading}>Login</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    //                     </div>
+    //                   <button className='bg-success form-control text-white logBtn' disabled={isLoading}>Login</button>
+    //                 </form>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
+    <AuthForm/>
   )
 }
 //eng.fatma.fateh@gmail.com
---
+++ src/Components/SharedUI/AuthForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function AuthForm({backGroundwidth}) {
+  return (
+    <>
+        <div className='background-container row'>
+            
+                <div className='col-lg-6 m-auto bg-white'>
+aaa
+                </div>
+        </div>
+    </>
+
+  )
+}
