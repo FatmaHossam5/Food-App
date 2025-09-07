8e2da9e41c41d43b2ca0cc4c478c57a8221be74a
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2025-09-07 14:10:13 -0400

    Remove configuration files and scripts for GitHub Pages deployment, including .eslintrc.cjs, .gitignore, and deploy.yml. Update package.json and vite.config.js for simplified build process.

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Food-App/' : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          ui: ['react-bootstrap', 'bootstrap']
-        }
-      }
-    }
-  }
+  base:'/',
+
 })
